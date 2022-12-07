0ac086d12f0da996558014b4e672f1b80fe8dd3b
ABB65 <bayhanbayramoglu@gmail.com>
2022-12-07 16:44:57 +0300

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,17 @@ export default defineNuxtConfig({
   css: ['remixicon/fonts/remixicon.css', '~/assets/css/tailwind.css'],
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   // https://content.nuxtjs.org
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Contentrain fonbulucu demo duyuru sitesi.',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Contentrain fonbulucu demo site.' }
+      ]
+    }
+  },
   content: {
     // navigation: {
     //   fields: ['navTitle']
